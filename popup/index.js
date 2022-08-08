b72c11054e7d710cf23b57d51a93ae6d870e1b20
nobodyzxc <nobodyzxc.tw@gmail.com>
2022-08-08 21:10:12 +0800

    add quote limit warning

--- popup/index.js
+++ popup/index.js
@@ -2115,7 +2115,7 @@ chrome.runtime.onMessage.addListener((req, sender, callback) => {
           pop_value(
             'bio_cookies',
             (([pro, cookies], idx, ary) => c2sess(cookies) == valueSelected),
-            (res, cookies) => redraw_bios(cookies)
+            (res, cookies) => redraw_bios(cookies, renew_profile = true)
           )
         }
       })

fda80d4baf24cb4084907ba775c8c0afa317ed63
nobodyzxc <nobodyzxc.tw@gmail.com>
2022-09-16 00:18:27 +0800

    fix icon selection

--- content/index.js
+++ content/index.js
@@ -32,32 +32,31 @@ if(!isLockedUser){
             { setCookies: true, cookies: config['cookie'] }
           )
         }
-      );
-      chrome.storage.local.get('plugins', (config)=>{
-        if(config['plugins']){
-          Object.keys(config['plugins']).forEach(name => {
-            let [mode, loc, enable, ctx] = config['plugins'][name];
-            if(enable && loc == "login"){
-              if(mode == 'url') plugTag('script', { src: ctx, })
-              else plugTag('script', { textContent: ctx, })
-            }
-            //else alert(enable, loc)
-          })
-        }
       });
-    });
-
-    chrome.runtime.onMessage.addListener((req, sender, callback) => {
-      if(req.cookieDone){
-        ajaxProfile(function(p, err){
-          if(p) window.location.href = "https://drrr.com/lounge/";
-          else{
-            //alert(`Bio Expired ${JSON.stringify(err)}`);
-            chrome.storage.sync.remove(['profile', 'cookie']);
-            chrome.runtime.sendMessage({expired_bio: true});
+    chrome.storage.local.get('plugins', (config)=>{
+      if(config['plugins']){
+        Object.keys(config['plugins']).forEach(name => {
+          let [mode, loc, enable, ctx] = config['plugins'][name];
+          if(enable && loc == "login"){
+            if(mode == 'url') plugTag('script', { src: ctx, })
+            else plugTag('script', { textContent: ctx, })
           }
-        }, true, 'login');
+          //else alert(enable, loc)
+        })
       }
     });
-  }
+  });
+
+  chrome.runtime.onMessage.addListener((req, sender, callback) => {
+    if(req.cookieDone){
+      ajaxProfile(function(p, err){
+        if(p) window.location.href = "https://drrr.com/lounge/";
+        else{
+          //alert(`Bio Expired ${JSON.stringify(err)}`);
+          chrome.storage.sync.remove(['profile', 'cookie']);
+          chrome.runtime.sendMessage({expired_bio: true});
+        }
+      }, true, 'login');
+    }
+  });
 }
